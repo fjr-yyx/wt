2d4c8b1c312ce039ce4862fd435bf00e3badd61d
Edward <necpos@foxmail.com>
2022-07-25 18:24:11 +0800

    hhh v4

--- src/main/java/com/example/pojo/demo01.java
+++ src/main/java/com/example/pojo/demo01.java
@@ -4,5 +4,6 @@ public class demo01 {
     public static void main(String[] args) {
         System.out.println("hello v1");
         System.out.println("hello v2");
+        System.out.println("hello v4");
     }
-            }
+}
